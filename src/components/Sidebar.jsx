9e823ceee42fc0008ae74e765f40210a2683b9b2
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-04-03 11:54:09 +0530

    batches and subject working responsively

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -64,66 +64,66 @@ const Sidebar = () => {
 
   return (
     <>
-    {isOpen ? (
-      <div className="fixed w-64 h-screen bg-gray-900 text-white">
-      <div className="">
-        <h2 className='text-2xl text-center'>  <IoMenu className='mt-4 ml-4' onClick={() => setIsOpen(!isOpen)}/> Admin Panel</h2>
-        <img className='w-50 mx-auto' src="/logo.png" alt="logo" />
-       
-      </div>
-      <nav className="mt-4">
-        <ul className="space-y-2">
-          {menus.map((menu) => (
-            <li key={menu.id}>
-              <button
-                onClick={() => {
-                  if (menu.id === 'teachers') {
-                    setShowTeacherSubmenu(!showTeacherSubmenu); // Toggle submenu visibility
-                  } else {
-                    router.push(menu.route);
-                    setActiveMenu(menu.id);
-                  }
-                }}
-                className={`w-full flex items-center space-x-2 p-2 rounded ${router.pathname === menu.route || activeMenu === menu.id
-                    ? 'bg-gray-700 text-white'
-                    : 'hover:bg-gray-700'
-                  }`}
-              >
-                <span className="text-lg">{menu.icon}</span>
-                <span>{menu.label}</span>
-              </button>
-              {menu.submenu && showTeacherSubmenu && (
-                <ul className="ml-4 mt-2 space-y-1">
-                  {menu.submenu.map((subMenu) => (
-                    <li key={subMenu.id}>
-                      <button
-                        onClick={() => {
-                          router.push(subMenu.route);
-                          setActiveMenu(subMenu.id);
+      {isOpen ? (
+        <div className="fixed w-64 h-screen bg-gray-900 text-white">
+          <div className="">
+            <h2 className='text-2xl text-center'>  <IoMenu className='mt-4 ml-4' onClick={() => setIsOpen(!isOpen)} /> Admin Panel</h2>
+            <img className='w-50 mx-auto' src="/logo.png" alt="logo" />
 
-                        }}
-                        className={`w-full flex items-center space-x-2 p-2 rounded ${router.pathname === subMenu.route || activeMenu === subMenu.id
-                            ? 'bg-gray-600 text-white'
-                            : 'hover:bg-gray-600'
-                          }`}
-                      >
-                        <span>{subMenu.label}</span>
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
-    ) : (
-      <div className="text-2xl mt-4 ml-4">
-        <IoMenu onClick={() => setIsOpen(!isOpen)}/>
-      </div>
+          </div>
+          <nav className="mt-4">
+            <ul className="space-y-2">
+              {menus.map((menu) => (
+                <li key={menu.id}>
+                  <button
+                    onClick={() => {
+                      if (menu.id === 'teachers') {
+                        setShowTeacherSubmenu(!showTeacherSubmenu); // Toggle submenu visibility
+                      } else {
+                        router.push(menu.route);
+                        setActiveMenu(menu.id);
+                      }
+                    }}
+                    className={`w-full flex items-center space-x-2 p-2 rounded ${router.pathname === menu.route || activeMenu === menu.id
+                      ? 'bg-gray-700 text-white'
+                      : 'hover:bg-gray-700'
+                      }`}
+                  >
+                    <span className="text-lg">{menu.icon}</span>
+                    <span>{menu.label}</span>
+                  </button>
+                  {menu.submenu && showTeacherSubmenu && (
+                    <ul className="ml-4 mt-2 space-y-1">
+                      {menu.submenu.map((subMenu) => (
+                        <li key={subMenu.id}>
+                          <button
+                            onClick={() => {
+                              router.push(subMenu.route);
+                              setActiveMenu(subMenu.id);
+
+                            }}
+                            className={`w-full flex items-center space-x-2 p-2 rounded ${router.pathname === subMenu.route || activeMenu === subMenu.id
+                              ? 'bg-gray-600 text-white'
+                              : 'hover:bg-gray-600'
+                              }`}
+                          >
+                            <span>{subMenu.label}</span>
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      ) : (
+        <div className="fixed text-2xl mt-4 ml-4">
+          <IoMenu onClick={() => setIsOpen(!isOpen)} />
+        </div>
       )}
-      
+
     </>
   );
 };
