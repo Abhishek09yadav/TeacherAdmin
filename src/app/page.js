37257a80d70960ec437522514c09ae170bb28312
Abhishek09yadav <monuy8830@gmail.com>
2025-04-03 19:19:41 +0530

    fix: comment out teacher options mapping to prevent rendering issues

--- src/app/page.js
+++ src/app/page.js
@@ -108,11 +108,11 @@ export default function Home() {
           className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         >
           <option value="">All Teachers</option>
-          {teachers.map((teacher) => (
+          {/* {teachers.map((teacher) => (
             <option key={teacher.id} value={teacher.name}>
               {teacher.name}
             </option>
-          ))}
+          ))} */}
         </select>
       </div>
 
