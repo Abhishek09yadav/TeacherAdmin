f208dfabe1bb0ee76932fdd96f0dc6ae37193334
Abhishek Yadav <monuy8830@gmail.com>
2025-04-11 15:36:13 +0530

    style: format code for consistency and improve readability in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { ImExit } from 'react-icons/im';
-import { toast } from 'react-toastify';
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { ImExit } from "react-icons/im";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
   const router = useRouter();
@@ -38,7 +38,6 @@ const Navbar = () => {
     <>
       {/* Top Navbar */}
       <nav className=" bg-gray-900 h-[70px] w-full flex items-center px-4">
-
         {/* Spacer to push logout to right */}
         <div className="flex justify-end w-full">
           {isLoggedIn && (
@@ -55,16 +54,19 @@ const Navbar = () => {
 
       {/* Confirmation Modal */}
       {showConfirmDialog && (
-        <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black/70 bg-opacity-60 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg p-6 max-w-sm w-full text-center">
             <h2 className="text-xl font-semibold mb-3">Confirm Logout</h2>
-            <p className="text-gray-600 mb-6">Are you sure you want to log out?</p>
+            <p className="text-gray-600 mb-6">
+              Are you sure you want to log out?
+            </p>
             <div className="flex justify-center gap-4">
               <button
                 onClick={confirmLogout}
                 className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
                 style={{
-                  boxShadow:"inset 2px 2px 2px #ad2929, inset -2px -2px 3px #ff8e8e"
+                  boxShadow:
+                    "inset 2px 2px 2px #ad2929, inset -2px -2px 3px #ff8e8e",
                 }}
               >
                 Logout
