765e48f23e45e28dd6aae52a47678109519a7e37
Abhishek Yadav <monuy8830@gmail.com>
2025-05-07 11:18:28 +0530

    feat: implement Subjects management with CRUD operations and integrate into Modules

--- src/components/Modules/Modules.jsx
+++ src/components/Modules/Modules.jsx
@@ -1,21 +1,21 @@
 "use client";
 import React, { useState } from "react";
 import PdfUploader from "./PdfUploader";
-
+import ModuleSubject from "./ModuleSubject";
 const Modules = () => {
   const [activeTab, setActiveTab] = useState("Classes");
 
-  const tabs = ["Classes", "Courses", "Sub Topic", "Upload PDF"];
+  const tabs = ["Classes", "Courses", "Subject", "Topic", "Upload PDF"];
 
   return (
     <div>
       {/* Navbar */}
-      <div className="flex space-x-4  text-sm   justify-center bg-blue-100 shadow">
+      <div className="flex md:space-x-4  text-xs md:text-sm  items-center  justify-center bg-blue-100 shadow">
         {tabs.map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={` px-4 py-2 
+            className={` px-2 md:px-4 py-2 
           rounded-md
           transition-all duration-200
           hover:bg-blue-200 ${
@@ -32,6 +32,7 @@ const Modules = () => {
 
       {/* Conditional Content */}
       {activeTab === "Upload PDF" && <PdfUploader />}
+      {activeTab === "Subject" && <ModuleSubject />}
     </div>
   );
 };
